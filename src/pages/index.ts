a39ab938c24f5b830a7d9486ea21d732d597d7ab
kasun751 <kasunp751@gmail.com>
2025-09-19 18:43:43 +0530

    Bug : solve bug

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,5 @@
 export { HomePage } from '@/pages/homepage'
 export { AboutUsPage } from '@/pages/aboutUsPage'
-<<<<<<< Updated upstream
-export { ContactUsPage } from '@/pages/contactUsPage'
-=======
 export { ContactUsPage } from '@/pages/contactUsPage'
 // export { LoginPage } from '@/pages/loginpage'
->>>>>>> Stashed changes
+
