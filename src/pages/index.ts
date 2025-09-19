47d066934895b96ec067bd322298b349ffbfef80
kasun751 <kasunp751@gmail.com>
2025-09-19 18:39:38 +0530

    add example .env file

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,8 @@
 export { HomePage } from '@/pages/homepage'
 export { AboutUsPage } from '@/pages/aboutUsPage'
+<<<<<<< Updated upstream
 export { ContactUsPage } from '@/pages/contactUsPage'
+=======
+export { ContactUsPage } from '@/pages/contactUsPage'
+// export { LoginPage } from '@/pages/loginpage'
+>>>>>>> Stashed changes
