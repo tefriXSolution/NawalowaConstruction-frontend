9864aad29db44634a960782b259273b4c4a1c0b6
kasun751 <kasunp751@gmail.com>
2025-10-04 02:00:40 +0530

    Update Sidebar.jsx

--- src/layouts/adminDashboardLayout/components/SideBar.tsx
+++ src/layouts/adminDashboardLayout/components/SideBar.tsx
@@ -5,13 +5,11 @@ import {
   SidebarItemGroup,
   SidebarItems,
 } from "flowbite-react";
-import {
-  HiArrowSmRight,
-  HiHome,
-} from "react-icons/hi";
+import { HiHome } from "react-icons/hi";
 import { LuNotebookText } from "react-icons/lu";
 import { VscSettingsGear } from "react-icons/vsc";
 import { IoLogOutOutline } from "react-icons/io5";
+import { useNavigate, useLocation } from "react-router";
 
 const sidebarMenu = [
   { label: "Home", href: "/", icon: HiHome },
@@ -22,16 +20,42 @@ const sidebarMenu = [
 ];
 
 export const SideBar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <Sidebar aria-label="Admin Dashboard Sidebar" theme={dashboardTheme}>
-          <SidebarItems>
-            <SidebarItemGroup theme={dashboardTheme.itemGroup} className="flex flex-col justify-items-start">
-              {sidebarMenu.map((item) =><SidebarItem theme={dashboardTheme.item} key={item.label} href={item.href} icon={item.icon}>
-                    {item.label}
-                  </SidebarItem>
-              )}
-            </SidebarItemGroup>
-          </SidebarItems>
-        </Sidebar>
-  )
-}
+      <SidebarItems>
+        <SidebarItemGroup
+          theme={dashboardTheme.itemGroup}
+          className="flex flex-col justify-items-start"
+        >
+          {sidebarMenu.map((item) => (
+            <SidebarItem
+  key={item.label}
+  theme={dashboardTheme.item}
+  icon={() => (
+    <item.icon
+      className={`h-6 w-6 transition duration-75 ${
+        location.pathname === item.href
+          ? "text-mainSidebar-color"
+          : "text-mainSidebar-link-hover-color group-hover:text-mainSidebar-linkText-hover-color"
+      }`}
+    />
+  )}
+  onClick={() => navigate(item.href)}
+  className={`cursor-pointer ${
+    location.pathname === item.href
+      ? "!bg-mainSidebar-link-hover-color !text-mainSidebar-color font-semibold"
+      : ""
+  }`}
+>
+  {item.label}
+</SidebarItem>
+
+          ))}
+        </SidebarItemGroup>
+      </SidebarItems>
+    </Sidebar>
+  );
+};
